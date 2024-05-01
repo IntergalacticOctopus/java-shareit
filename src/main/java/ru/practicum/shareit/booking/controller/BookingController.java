f5480e4599e67a9ca1670a7f2e24f96d602f926f
Ura <alexanoffura1@mail.ru>
2024-05-02 02:39:38 +0300

    Удалены TODO

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -10,9 +10,6 @@ import ru.practicum.shareit.booking.service.BookingService;
 import javax.validation.Valid;
 import java.util.List;
 
-/**
- * TODO Sprint add-bookings.
- */
 @RestController
 @RequestMapping(path = "/bookings")
 @Slf4j
