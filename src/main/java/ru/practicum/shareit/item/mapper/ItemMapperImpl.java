acec6dffa031e497c9272dd9480d6e9c4dbb4c2e
Ura <alexanoffura1@mail.ru>
2024-05-12 22:03:37 +0300

    Добавлены тесты

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapperImpl.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapperImpl.java
@@ -6,6 +6,7 @@ import ru.practicum.shareit.item.dto.ItemCreateDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemUpdateDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.model.Request;
 import ru.practicum.shareit.request.storage.RequestRepository;
 import ru.practicum.shareit.user.model.User;
 
@@ -16,27 +17,21 @@ public class ItemMapperImpl implements ItemMapper {
 
     @Override
     public Item toItem(ItemUpdateDto item, User owner) {
-        if (item.getRequestId() != null) {
-            return new Item(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), owner, requestRepository.findById(item.getRequestId()).orElse(null));
-        }
-        return new Item(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), owner, null);
+        Long requestId = item.getRequestId();
+        Request request = requestId!= null? requestRepository.findById(requestId).orElse(null) : null;
+        return new Item(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), owner, request);
     }
 
     @Override
     public Item toItem(ItemCreateDto item, User owner) {
-        if (item.getRequestId() != null) {
-            return new Item(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), owner, requestRepository.findById(item.getRequestId()).orElse(null));
-        }
-        return new Item(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), owner, null);
+        Long requestId = item.getRequestId();
+        Request request = requestId!= null? requestRepository.findById(requestId).orElse(null) : null;
+        return new Item(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), owner, request);
     }
 
     @Override
     public ItemDto toItemDto(Item item) {
-        if (item.getRequest() != null) {
-            return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), item.getRequest().getId());
-        }
-        return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), null);
+        Long requestId = item.getRequest()!= null? item.getRequest().getId() : null;
+        return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.getAvailable(), requestId);
     }
-
-
 }
