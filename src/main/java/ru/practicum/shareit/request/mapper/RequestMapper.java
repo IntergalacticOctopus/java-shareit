3d54c8ca07ff0623a15bacdf4ba6a390c6e77b34
Ura <alexanoffura1@mail.ru>
2024-05-12 21:44:24 +0300

    Добавлены тесты

--- src/main/java/ru/practicum/shareit/request/mapper/RequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/mapper/RequestMapper.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.storage.ItemRepository;
 import ru.practicum.shareit.request.dto.RequestCreateDto;
 import ru.practicum.shareit.request.dto.RequestDto;
