1b3a6706c7a4bc760d4915a902f08d06262dabd0
Ura <alexanoffura1@mail.ru>
2024-05-15 05:14:20 +0300

    Внесены правки

--- gateway/src/main/java/ru/practicum/shareit/request/dto/RequestCreateDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/RequestCreateDto.java
@@ -5,18 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestCreateDto {
-    private Long id;
-
     @NotEmpty
     private String description;
-
-    private long requesterId;
-
-    private LocalDateTime created;
 }
